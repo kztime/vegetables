8b305e48f71b2016dbae44febea910524888e0b0
Sergey Demidov <aspirin_1988@mail.ru>
2016-11-24 20:30:20 +0600

    Создал класс Vegetable для получения и сохранения свойств овоща

---
+++ Vegetable.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: serg
+ * Date: 24.11.16
+ * Time: 20:09
+ */
+class Vegetable
+{
+
+    /**
+     * @var Integer
+     */
+    protected $price;
+
+    /**
+     * @var String
+     */
+    protected $color;
+
+    /**
+     * @var Integer
+     */
+    protected $size;
+
+    public function __construct(array $vegetable)
+    {
+        $this->setColor($vegetable['color']);
+        $this->setPrice($vegetable['price']);
+        $this->setSize($vegetable['size']);
+    }
+
+    /**
+     * @return int
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * @param int $size
+     * @return Vegetable
+     */
+    public function setSize($size)
+    {
+        $this->size = $size;
+        return $this;
+    }
+
+    /**
+     * @return String
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param String $color
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param int $price
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function getOptions()
+    {
+        return ['color', 'price', 'size'];
+    }
+
+    public function get()
+    {
+        return [
+            'color' => $this->getColor(),
+            'price' => $this->getSize(),
+            'size' => $this->getSize()
+        ];
+    }
+}
